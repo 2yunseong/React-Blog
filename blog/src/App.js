c49c9699c52d9456725e53ca6d002fc27544a544
yunseong9999 <dbsdltjd3701@naver.com>
2021-11-04 01:51:19 +0900

    feat: 초기세팅

---
+++ blog/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <div
+          style={{
+            color: "blue",
+            fontSize: "30px",
+          }}
+        >
+          {" "}
+          윤성 Devlog
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
